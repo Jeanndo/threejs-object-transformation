41786d34578cc0bb7d91ead52480b5c7c7e72082
Angelu123 <izerefaifelix@gmail.com>
2022-11-26 20:52:45 +0200

    grouping objects

--- src/script.js
+++ src/script.js
@@ -11,17 +11,17 @@ const scene = new THREE.Scene();
  * Objects
  */
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
 //position
 
 // mesh.position.x = 0.7;
 // mesh.position.y = -0.6;
 // mesh.position.z = 1;
 
-mesh.position.set(0.7, -0.6, 1); // short for positioning objects in 3D
+// mesh.position.set(0.7, -0.6, 1); // short for positioning objects in 3D
 
 //Scale
 
@@ -29,15 +29,48 @@ mesh.position.set(0.7, -0.6, 1); // short for positioning objects in 3D
 // mesh.scale.y = 0.5;
 // mesh.scale.z = 0.5;
 
-mesh.scale.set(2, 0.5, 0.5); // shorthand for scaling
+// mesh.scale.set(2, 0.5, 0.5); // shorthand for scaling
 
 //rotation
 
-mesh.rotation.reorder("YXZ");
+// mesh.rotation.reorder("YXZ");
 // mesh.rotation.y = Math.PI;
 // mesh.rotation.y = 3.14159;
-mesh.rotation.x = Math.PI * 0.25;
-mesh.rotation.y = Math.PI * 0.25;
+// mesh.rotation.x = Math.PI * 0.25;
+// mesh.rotation.y = Math.PI * 0.25;
+
+const group = new THREE.Group();
+
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 1;
+
+scene.add(group);
+
+const cub1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+
+group.add(cub1);
+
+const cub2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+
+cub2.position.x = -2;
+
+group.add(cub2);
+
+const cub3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+
+cub3.position.x = 2;
+
+group.add(cub3);
 
 // AXES HELPER
 
